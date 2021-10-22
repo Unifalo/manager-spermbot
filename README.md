83c292f542e62d39b966f12a829fedd96286c585
Unifalo <tuyetnhi133a@gmail.com>
2021-10-22 11:46:35 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# manager-spermbot
+# Termux Manager
+![Demo](https://i.imgur.com/mK1LeIu.png)
+## Install
+```
+cd ~
+pkg install wget php
+wget https://raw.githubusercontent.com/unifalo/manager-spermbot/master/manager.php
+bash install.sh
+```
+## Usage
+Run `manager`
---
+++ install.sh
@@ -0,0 +1,18 @@
+# >_ Copyright by Gingdev 2020
+
+ROOT=/data/data/com.termux/files
+ADDR=127.0.0.1:9876
+BIN=$PREFIX/bin/manager
+
+# Download script file
+wget https://raw.githubusercontent.com/unifalo/manager-spermbot/master/manager.php -O $ROOT/index.php
+
+# Set up alias
+cat > $BIN <<EOF
+cd $ROOT
+php -S $ADDR &
+xdg-open http://$ADDR
+wait
+EOF
+
+chmod +x $BIN
